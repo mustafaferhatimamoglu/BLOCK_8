0cf478cdddae01e95f0483d7ea54a5cdb0d3ee3b
mustafa ferhat <mustafaferhat51800@outlook.com>
2024-09-03 23:53:29 +0300

    .

--- Form1.cs
+++ Form1.cs
@@ -2,20 +2,6 @@ namespace BLOCK_8
 {
     public partial class Form1 : Form
     {
-        public void Setup_Form_Right()
-        {
-            Screen scr = Screen.FromPoint(this.Location);
-            this.Location = new Point(scr.WorkingArea.Width / 2, scr.WorkingArea.Top);
-            this.Width = scr.WorkingArea.Width / 2;
-            this.Height = scr.WorkingArea.Height;
-        }
-        public void Setup_Form_Right_2K()
-        {
-            Screen scr = Screen.FromPoint(this.Location);
-            this.Location = new Point(1275, 0);
-            this.Width = 1292;
-            this.Height = 1407;
-        }
         private void Setups()
         {
 
@@ -29,7 +15,7 @@ namespace BLOCK_8
 
         private void This_Load(object? sender, EventArgs e)
         {
-            Setup_Form_Right_2K();
+            CommonFunctions.Form.Setup_Form_Right_2K(this);
         }
 
         private void button1_Click(object sender, EventArgs e)
